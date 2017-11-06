52f2bd64845c833097d0e0c53ac9a8b77886e10c
Michael <michael.niedermayr@gmail.com>
2017-11-06 17:44:15 +0100

    Keba - new icons, speech

--- app/src/main/java/com/keba/keba/MainActivity.java
+++ app/src/main/java/com/keba/keba/MainActivity.java
@@ -1,7 +1,10 @@
 package com.keba.keba;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.AlarmClock;
+import android.speech.RecognizerIntent;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -15,10 +18,15 @@ import android.widget.Toast;
 import com.keba.keba.BarcodeUtil.BarcodeIntentIntegrator;
 import com.keba.keba.BarcodeUtil.BarcodeIntentResult;
 
+import java.util.ArrayList;
+import java.util.Locale;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final int REQ_CODE_SPEECH_INPUT = 100;
 
     @BindView(R.id.main_contentText) TextView contentText;
 
@@ -35,7 +43,8 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                onClickQrButton();
+                promptSpeechInput();
+                //onClickQrButton();
             }
         });
     }
@@ -69,11 +78,43 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
-        BarcodeIntentResult scanResult = BarcodeIntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
-        if (scanResult != null) {
-            String content = scanResult.getContents();
-            contentText.setText(content);
+    private void promptSpeechInput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, "de-DE");
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Say something...");
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getApplicationContext(), "speech not supported", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        switch (requestCode) {
+            case BarcodeIntentIntegrator.REQUEST_CODE:
+                BarcodeIntentResult scanResult = BarcodeIntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+                if (scanResult != null) {
+                    String content = scanResult.getContents();
+                    contentText.setText(content);
+                }
+                break;
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != intent) {
+                    ArrayList<String> result = intent.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+
+                    String response = result.get(0);
+                    contentText.setText(response);
+
+                    if(response.equals("Barcode")) {
+                        BarcodeIntentIntegrator barcodeIntentIntegrator = new BarcodeIntentIntegrator(this);
+                        barcodeIntentIntegrator.initiateScan();
+                    }
+                }
+                break;
+            }
         }
     }
 }
